a9424cc2f3cd2f3a15f9cf449dcececce81e6747
Xanyook <frspam@yopmail.com>
2016-11-06 20:23:08 +0100

    création du projet

---
+++ xabook/src/main/java/org/xanyook/xabook/App.java
@@ -0,0 +1,13 @@
+package org.xanyook.xabook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
