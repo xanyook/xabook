e8c5ab634af54679d41c51074d2b8b3ad2e8d5a4
Gil GOUGNOT <ggougnot.prestataire@voyages-sncf.com>
2017-02-17 18:13:45 +0100

    renommage classe

---
+++ src/main/java/org/xanyook/xabook/ApplicationLauncher.java
@@ -0,0 +1,27 @@
+package org.xanyook.xabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.xanyook.xabook.repository.XabookRepositoryImpl;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories(repositoryBaseClass = XabookRepositoryImpl.class)
+public class ApplicationLauncher extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+
+		// new Application().configure(new
+		// SpringApplicationBuilder(Application.class)).run(args);
+		SpringApplication.run(ApplicationLauncher.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApplicationLauncher.class);
+	}
+}
